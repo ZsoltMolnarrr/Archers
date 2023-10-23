b7cd0b2bc2bd06b20352efef5487f2881bae6cc1
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2023-10-23 13:32:54 +0200

    Fix autofire stutter

--- src/main/java/net/archers/mixin/client/autofire/LivingEntityMixin.java
+++ src/main/java/net/archers/mixin/client/autofire/LivingEntityMixin.java
@@ -15,9 +15,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
+    private int timeHoldingCharged = 0;
     @Inject(method = "tickItemStackUsage", at = @At("HEAD"))
     private void asd(ItemStack stack, CallbackInfo ci) {
         var entity = (LivingEntity) (Object) this;
+        var charged = false;
         if (entity.getWorld().isClient()) {
             if (entity == MinecraftClient.getInstance().player) {
                 var player = MinecraftClient.getInstance().player;
@@ -27,14 +29,19 @@ public class LivingEntityMixin {
                     if (predicate != null) {
                         var state = predicate.call(mainHandStack, (ClientWorld) entity.getWorld(), player, 1234);
                         if (state >= 1) {
-                            // Set weapon charged (in a synchronized way)
-                            MinecraftClient.getInstance().interactionManager.stopUsingItem(player);
-                            // Wait a little before firing (to make sure the server has time to process the packet)
-                            ((ItemUseDelay) MinecraftClient.getInstance()).imposeItemUseCD_Archers(2);
+                            charged = true;
+                            // 1 Extra tick to avoid releaseing earlier than server agrees on being charged
+                            if (timeHoldingCharged > 1) {
+                                // Set weapon charged (in a synchronized way)
+                                MinecraftClient.getInstance().interactionManager.stopUsingItem(player);
+                                // Wait a little before firing (to make sure the server has time to process the packet)
+                                ((ItemUseDelay) MinecraftClient.getInstance()).imposeItemUseCD_Archers(2);
+                            }
                         }
                     }
                 }
             }
         }
+        timeHoldingCharged = charged ? timeHoldingCharged + 1 : 0;
     }
 }
