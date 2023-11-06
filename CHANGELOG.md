ead5fff57d28670669604d876eda2b10d5d624ab
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2023-11-06 17:55:20 +0100

    Update version and changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+# 1.0.1
+
+- Update creative tab icon
+- Add Russian translation #1, thanks to skel39eek66 #1
+
 # 1.0.0
 
 - Initial release
--- src/main/java/net/archers/ArchersMod.java
+++ src/main/java/net/archers/ArchersMod.java
@@ -72,7 +72,7 @@ public class ArchersMod implements ModInitializer {
 
     private void registerItemGroup() {
         Group.ARCHERS = FabricItemGroup.builder()
-                .icon(() -> new ItemStack(Armors.archerArmorSet_T1.head.asItem()))
+                .icon(() -> new ItemStack(Armors.archerArmorSet_T2.head.asItem()))
                 .displayName(Text.translatable("itemGroup." + ID + ".general"))
                 .build();
         Registry.register(Registries.ITEM_GROUP, Group.KEY, Group.ARCHERS);
