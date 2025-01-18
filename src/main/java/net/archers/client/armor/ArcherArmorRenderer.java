ccb23ded9a4875b4e1aa00ce784d4fbc0421ad36
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2025-01-18 20:35:40 +0100

    Update Azure version, remove animator

--- src/main/java/net/archers/client/armor/ArcherArmorRenderer.java
+++ src/main/java/net/archers/client/armor/ArcherArmorRenderer.java
@@ -1,13 +1,9 @@
 package net.archers.client.armor;
 
-import mod.azure.azurelibarmor.rewrite.animation.controller.AzAnimationControllerContainer;
-import mod.azure.azurelibarmor.rewrite.animation.impl.AzItemAnimator;
 import mod.azure.azurelibarmor.rewrite.render.armor.AzArmorRenderer;
 import mod.azure.azurelibarmor.rewrite.render.armor.AzArmorRendererConfig;
 import net.archers.ArchersMod;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
-import org.jetbrains.annotations.NotNull;
 
 public class ArcherArmorRenderer extends AzArmorRenderer {
     public static ArcherArmorRenderer archer() {
@@ -20,27 +16,10 @@ public class ArcherArmorRenderer extends AzArmorRenderer {
         return new ArcherArmorRenderer("ranger_armor", "netherite_ranger_armor");
     }
 
-    public static class ArcherArmorAnimator extends AzItemAnimator {
-        private static final Identifier ANIMATIONS = Identifier.of(
-                ArchersMod.ID,
-                "animations/item/empty.animation.json"
-        );
-
-        @Override
-        public void registerControllers(AzAnimationControllerContainer<ItemStack> animationControllerContainer) {}
-
-        @Override
-        public @NotNull Identifier getAnimationLocation(ItemStack animatable) {
-            return ANIMATIONS;
-        }
-    }
-
     public ArcherArmorRenderer(String modelName, String textureName) {
         super(AzArmorRendererConfig.builder(
                 Identifier.of(ArchersMod.ID, "geo/" + modelName + ".geo.json"),
                 Identifier.of(ArchersMod.ID, "textures/armor/" + textureName + ".png")
-        )
-                .setAnimatorProvider(ArcherArmorAnimator::new)
-                .build());
+        ).build());
     }
 }
