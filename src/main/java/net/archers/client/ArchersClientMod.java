78ab3e707ac4b59c9da60e97dd13377b616c1537
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2025-01-18 19:37:24 +0100

    AzureLib migration

--- src/main/java/net/archers/client/ArchersClientMod.java
+++ src/main/java/net/archers/client/ArchersClientMod.java
@@ -1,20 +1,26 @@
 package net.archers.client;
 
+import mod.azure.azurelibarmor.rewrite.render.armor.AzArmorRenderer;
+import mod.azure.azurelibarmor.rewrite.render.armor.AzArmorRendererRegistry;
 import net.archers.ArchersMod;
 import net.archers.block.ArcherBlocks;
+import net.archers.client.armor.ArcherArmorRenderer;
 import net.archers.client.effect.HuntersMarkRenderer;
 import net.archers.client.effect.RootsRenderer;
 import net.archers.client.util.ArchersTooltip;
 import net.archers.effect.Effects;
+import net.archers.item.Armors;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.util.Identifier;
 import net.spell_engine.api.effect.CustomModelStatusEffect;
+import net.spell_engine.api.item.armor.Armor;
 import net.spell_engine.api.render.CustomModels;
 import net.spell_engine.client.gui.SpellTooltip;
 
 import java.util.List;
+import java.util.function.Supplier;
 
 public class ArchersClientMod implements ClientModInitializer {
     @Override
@@ -38,5 +44,13 @@ public class ArchersClientMod implements ClientModInitializer {
             description = description.replace(SpellTooltip.placeholder("damage_taken"), "" + huntersMarkPercent);
             return description;
         });
+
+        registerArmorRenderer(Armors.archerArmorSet_T1, ArcherArmorRenderer::archer);
+        registerArmorRenderer(Armors.archerArmorSet_T2, ArcherArmorRenderer::ranger);
+        registerArmorRenderer(Armors.archerArmorSet_T3, ArcherArmorRenderer::netheriteRanger);
+    }
+
+    private static void registerArmorRenderer(Armor.Set set, Supplier<AzArmorRenderer> armorRendererSupplier) {
+        AzArmorRendererRegistry.register(armorRendererSupplier, set.head, set.chest, set.legs, set.feet);
     }
 }
--- src/main/java/net/archers/item/armor/ArcherArmor.java
+++ src/main/java/net/archers/item/armor/ArcherArmor.java
@@ -1,69 +1,21 @@
 package net.archers.item.armor;
 
-import mod.azure.azurelibarmor.common.api.client.renderer.GeoArmorRenderer;
-import mod.azure.azurelibarmor.common.api.common.animatable.GeoItem;
-import mod.azure.azurelibarmor.common.internal.client.RenderProvider;
-import mod.azure.azurelibarmor.common.internal.common.util.AzureLibUtil;
-import mod.azure.azurelibarmor.core.animatable.instance.AnimatableInstanceCache;
-import mod.azure.azurelibarmor.core.animation.AnimatableManager;
-import net.archers.ArchersMod;
-import net.archers.client.armor.ArcherArmorRenderer;
-import net.minecraft.client.render.entity.model.BipedEntityModel;
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ArmorMaterial;
-import net.minecraft.item.ItemStack;
 import net.minecraft.registry.entry.RegistryEntry;
-import net.minecraft.util.Identifier;
 import net.spell_engine.api.item.armor.Armor;
 
-import java.util.function.Consumer;
-
-public class ArcherArmor extends Armor.CustomItem implements GeoItem {
+public class ArcherArmor extends Armor.CustomItem {
     public ArcherArmor(RegistryEntry<ArmorMaterial> material, Type slot, Settings settings) {
         super(material, slot, settings);
     }
-    public static final Identifier ARCHER_ARMOR_MODEL = Identifier.of(ArchersMod.ID, "archer_armor");
-    public static final Identifier RANGER_ARMOR_MODEL = Identifier.of(ArchersMod.ID, "ranger_armor");
-
-    public Identifier customModelId = ARCHER_ARMOR_MODEL;
 
     public static ArcherArmor archer(RegistryEntry<ArmorMaterial> material, Type slot, Settings settings) {
         var armor = new ArcherArmor(material, slot, settings);
-        armor.customModelId = ARCHER_ARMOR_MODEL;
         return armor;
     }
 
     public static ArcherArmor ranger(RegistryEntry<ArmorMaterial> material, Type slot, Settings settings) {
         var armor = new ArcherArmor(material, slot, settings);
-        armor.customModelId = RANGER_ARMOR_MODEL;
         return armor;
-    }
-
-    // MARK: GeoItem
-
-    @Override
-    public void createRenderer(Consumer<RenderProvider> consumer) {
-        consumer.accept(new RenderProvider() {
-            private GeoArmorRenderer<?> renderer;
-            @Override
-            public BipedEntityModel<LivingEntity> getHumanoidArmorModel(LivingEntity livingEntity, ItemStack itemStack, EquipmentSlot equipmentSlot, BipedEntityModel<LivingEntity> original) {
-                if (this.renderer == null) {
-                    this.renderer = new ArcherArmorRenderer();
-                }
-                this.renderer.prepForRender(livingEntity, itemStack, equipmentSlot, original);
-                return this.renderer;
-            }
-        });
-    }
-
-    @Override
-    public void registerControllers(AnimatableManager.ControllerRegistrar controllers) { }
-
-    private final AnimatableInstanceCache cache = AzureLibUtil.createInstanceCache(this);
-
-    @Override
-    public AnimatableInstanceCache getAnimatableInstanceCache() {
-        return cache;
     }
 }
