ca1f3a39fa5b2fd3b6cb70bc3464352b09d1cf99
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2025-03-27 12:01:12 +0100

    Attempt fix TexturedRenderLayers/renderer.Sheets early load

--- src/main/java/net/archers/client/effect/RootsRenderer.java
+++ src/main/java/net/archers/client/effect/RootsRenderer.java
@@ -2,7 +2,6 @@ package net.archers.client.effect;
 
 import net.archers.ArchersMod;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.TexturedRenderLayers;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.util.math.MatrixStack;
@@ -14,14 +13,13 @@ import net.spell_engine.api.render.CustomModels;
 public class RootsRenderer implements CustomModelStatusEffect.Renderer {
 
     // MARK: Renderer
-    private static final RenderLayer RENDER_LAYER = TexturedRenderLayers.getEntityCutout();
 
     public static final Identifier modelId = Identifier.of(ArchersMod.ID, "effect/entangling_roots");
     @Override
     public void renderEffect(int amplifier, LivingEntity livingEntity, float delta, MatrixStack matrixStack, VertexConsumerProvider vertexConsumers, int light) {
         matrixStack.push();
         matrixStack.translate(0, 0.5, 0);
-        CustomModels.render(RENDER_LAYER, MinecraftClient.getInstance().getItemRenderer(), modelId,
+        CustomModels.render(TexturedRenderLayers.getEntityCutout(), MinecraftClient.getInstance().getItemRenderer(), modelId,
                 matrixStack, vertexConsumers, light, livingEntity.getId());
         matrixStack.pop();
     }
