c2c7cbe7948051540ced55b7aff4104fd26815b1
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2025-04-08 22:35:03 +0200

    Unsmelt armor and weapons

--- src/main/java/net/archers/datagen/ArchersDataGenerator.java
+++ src/main/java/net/archers/datagen/ArchersDataGenerator.java
@@ -8,11 +8,19 @@ import net.archers.item.Weapons;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
 import net.spell_engine.api.datagen.SimpleSoundGeneratorV2;
 import net.spell_engine.api.datagen.SpellGenerator;
+import net.spell_engine.api.item.armor.Armor;
 import net.spell_engine.rpg_series.datagen.RPGSeriesDataGen;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class ArchersDataGenerator implements DataGeneratorEntrypoint {
@@ -22,6 +30,7 @@ public class ArchersDataGenerator implements DataGeneratorEntrypoint {
         pack.addProvider(SoundGen::new);
         pack.addProvider(SpellGen::new);
         pack.addProvider(ItemTagGenerator::new);
+        pack.addProvider(UnsmeltGenerator::new);
     }
 
     public static class ItemTagGenerator extends RPGSeriesDataGen.ItemTagGenerator {
@@ -65,6 +74,100 @@ public class ArchersDataGenerator implements DataGeneratorEntrypoint {
                                     .map(entry -> SoundEntry.withVariants(entry.id().getPath(), entry.variants()))
                                     .toList()
                     )
+            );
+        }
+    }
+
+    public static class UnsmeltGenerator extends FabricRecipeProvider {
+        public UnsmeltGenerator(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+            super(output, registriesFuture);
+        }
+
+        public static int UNSMELT_TIME = 300;
+
+        @Override
+        public void generate(RecipeExporter exporter) {
+            disassembleArmor(exporter, Armors.archerArmorSet_T1, Items.LEATHER);
+            disassembleArmor(exporter, Armors.archerArmorSet_T2, Items.TURTLE_SCUTE);
+            disassembleArmor(exporter, Armors.archerArmorSet_T3, Items.NETHERITE_SCRAP);
+
+            disassemble(exporter,
+                    Weapons.meleeEntries.stream()
+                            .filter(entry -> entry.id().getPath().contains("flint"))
+                            .map(entry -> (ItemConvertible) entry.item()).toList(),
+                    Items.FLINT);
+            disassemble(exporter,
+                    Weapons.meleeEntries.stream()
+                            .filter(entry -> entry.id().getPath().contains("gold"))
+                            .map(entry -> (ItemConvertible) entry.item()).toList(),
+                    Items.IRON_NUGGET);
+            disassemble(exporter,
+                    Weapons.meleeEntries.stream()
+                            .filter(entry -> entry.id().getPath().contains("iron"))
+                            .map(entry -> (ItemConvertible) entry.item()).toList(),
+                    Items.GOLD_NUGGET);
+            disassemble(exporter,
+                    Weapons.meleeEntries.stream()
+                            .filter(entry -> entry.id().getPath().contains("diamond"))
+                            .map(entry -> (ItemConvertible) entry.item()).toList(),
+                    Items.DIAMOND);
+            disassemble(exporter,
+                    Weapons.meleeEntries.stream()
+                            .filter(entry -> entry.id().getPath().contains("netherite"))
+                            .map(entry -> (ItemConvertible) entry.item()).toList(),
+                    Items.NETHERITE_SCRAP);
+
+            disassemble(exporter,
+                    List.of(Weapons.mechanic_shortbow.item(), Weapons.rapid_crossbow.item()),
+                    Items.REDSTONE);
+            disassemble(exporter,
+                    List.of(Weapons.royal_longbow.item()),
+                    Items.GOLD_NUGGET);
+            disassemble(exporter,
+                    List.of(Weapons.heavy_crossbow.item()),
+                    Items.IRON_NUGGET);
+            disassemble(exporter,
+                    Weapons.rangedEntries.stream()
+                            .filter(entry -> entry.id().getPath().contains("netherite"))
+                            .map(entry -> (ItemConvertible) entry.item()).toList(),
+                    Items.NETHERITE_SCRAP);
+        }
+
+        private static void disassembleArmor(RecipeExporter exporter, Armor.Set armorSet, Item output) {
+            FabricRecipeProvider.offerSmelting(exporter,
+                    armorSet.pieces(),
+                    RecipeCategory.MISC,
+                    output,
+                    0.1f,
+                    UNSMELT_TIME,
+                    "disassemble"
+            );
+            FabricRecipeProvider.offerBlasting(exporter,
+                    armorSet.pieces(),
+                    RecipeCategory.MISC,
+                    output,
+                    0.1f,
+                    UNSMELT_TIME / 2,
+                    "disassemble"
+            );
+        }
+
+        private static void disassemble(RecipeExporter exporter, List<ItemConvertible> items, Item output) {
+            FabricRecipeProvider.offerSmelting(exporter,
+                    items,
+                    RecipeCategory.MISC,
+                    output,
+                    0.1f,
+                    UNSMELT_TIME,
+                    "disassemble"
+            );
+            FabricRecipeProvider.offerBlasting(exporter,
+                    items,
+                    RecipeCategory.MISC,
+                    output,
+                    0.1f,
+                    UNSMELT_TIME / 2,
+                    "disassemble"
             );
         }
     }
