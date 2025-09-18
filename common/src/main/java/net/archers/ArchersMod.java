2d84053198b088f354fa9b439d3dee67a69dd9a2
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2025-09-18 13:36:14 +0200

    Untweaks

--- common/src/main/java/net/archers/ArchersMod.java
+++ common/src/main/java/net/archers/ArchersMod.java
@@ -91,7 +91,6 @@ public class ArchersMod {
     }
 
     public static void registerItems() {
-        tweaksConfig.refresh();
         itemConfig.refresh();
         Group.ARCHERS = FabricItemGroup.builder()
                 .icon(() -> new ItemStack(Armors.archerArmorSet_T2.head.asItem()))
