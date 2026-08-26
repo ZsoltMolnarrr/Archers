a48db6a5001212b627905baae8e92df554ed0353
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2026-08-27 01:08:16 +0200

    Prepare sources for mapping migration

--- common/src/main/java/net/archers/client/entity/DirewolfEntityModel.java
+++ common/src/main/java/net/archers/client/entity/DirewolfEntityModel.java
@@ -2,7 +2,13 @@ package net.archers.client.entity;
 
 import net.archers.ArchersMod;
 import net.archers.client.render.ArcherRenderLayers;
-import net.minecraft.client.model.*;
+import net.minecraft.client.model.Dilation;
+import net.minecraft.client.model.ModelData;
+import net.minecraft.client.model.ModelPart;
+import net.minecraft.client.model.ModelPartBuilder;
+import net.minecraft.client.model.ModelPartData;
+import net.minecraft.client.model.ModelTransform;
+import net.minecraft.client.model.TexturedModelData;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.RenderLayers;
 import net.minecraft.client.render.entity.animation.Animation;
